3233e4bf5173e00b737fa782a2bd3b82514acf4e
tonysoul <tonysoul@foxmail.com>
2025-04-11 12:46:49 +0800

    mail-view

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import *
+
+class EmailAdmin(admin.ModelAdmin):
+  list_display = [field.name for field in Email._meta.fields]
+
+
 # Register your models here.
+
+admin.site.register(Email, EmailAdmin)
+admin.site.register(User)
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
   load_mailbox("inbox");
 
   // bind form submint event
-  send_mail();
+  bind_send_mail();
 });
 
 function compose_email() {
@@ -35,14 +35,16 @@ function load_mailbox(mailbox) {
   }</h3>`;
 
   if (mailbox === "inbox") {
+    load_mails("inbox");
   } else if (mailbox === "sent") {
-    load_sent_mails();
+    load_mails("sent");
   } else if (mailbox === "archive") {
+    load_mails("archive");
   }
 }
 
 // send mail
-function send_mail() {
+function bind_send_mail() {
   document.querySelector("#compose-form").onsubmit = function () {
     const recipients = document.querySelector("#compose-recipients").value;
     const subject = document.querySelector("#compose-subject").value;
@@ -72,6 +74,7 @@ function send_mail() {
 
 function show_errors(msg) {
   const errors = document.querySelector("#errors");
+  errors.style.display = "block";
   errors.innerHTML = msg;
   errors.classList.remove("d-none");
 }
@@ -82,17 +85,28 @@ function hide_errors() {
   errors.classList.add("d-none");
 }
 
-function load_sent_mails() {
+function load_mails(type) {
   const views = document.querySelector("#emails-view");
+  let url = "";
 
-  fetch("/emails/sent")
+  if (type === "inbox") {
+    url = "/emails/inbox";
+  } else if (type === "sent") {
+    url = "/emails/sent";
+  } else if (type === "archive") {
+    url = "emails/archive";
+  }
+
+  fetch(url)
     .then((response) => response.json())
     .then((result) => {
       if (result.length > 0) {
         // create str html template, it's simple
         let render_list_str = "";
         result.forEach((item) => {
-          const item_str = `<div class="d-flex mailItem border-bottom">
+          const item_str = `<div data-id="${
+            item.id
+          }" class="d-flex mailItem border-bottom ${item.read ? "gray" : ""}">
         <div class="mailItem__sender">${item.sender}</div>
         <div class="flex-fill">${item.subject}</div>
         <div class="mailItem__timestamp">${item.timestamp}</div>
@@ -103,20 +117,55 @@ function load_sent_mails() {
         views.insertAdjacentHTML("beforeend", render_list_str);
 
         // bind click event
-        document.querySelectorAll(".mailItem").forEach((item) => {
-          item.addEventListener("click", () => {
-            show_email_entity();
+        document.querySelectorAll(".mailItem").forEach(function (item) {
+          item.addEventListener("click", function () {
+            show_email_entity(this.dataset.id);
+            mark_email_status(this.dataset.id, { read: true });
           });
         });
       }
     });
 }
 
-function show_email_entity() {
+function show_email_entity(id) {
   clean_view();
   const entity_email = document.querySelector("#email-entity-view");
   entity_email.style.display = "block";
-  entity_email.innerHTML = "123";
+
+  fetch(`/emails/${id}`)
+    .then((response) => response.json())
+    .then((result) => {
+      if (!result.error) {
+        const entity_email_str_html = `
+  <div class="emailBox">
+        <div class="emailBox__hd">
+            <h4>${result.subject}</h4>
+            <ul class="emailBox__ul">
+                <li>sender: ${result.sender}</li>
+                <li>timestamp: ${result.timestamp}</li>
+                <li>recipients: ${result.recipients.join(", ")}</li>
+            </ul>
+        </div>  
+        <div class="emailBox__bd">
+            ${result.body}
+        </div>
+        <div class="emailBox__ft"></div>
+    </div>`;
+
+        entity_email.innerHTML = entity_email_str_html;
+      }
+    });
+}
+
+// obj => {read: true/false, archived: true/false}
+function mark_email_status(id, obj) {
+  fetch(`/emails/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(obj),
+  }).catch((error) => {
+    console.error(error);
+    show_errors(error);
+  });
 }
 
 // close all views
